72fb35fc8c5ab909be21f219ef421ebd2931bccb
armand chariot <armand@chariot.be>
2020-06-08 15:11:53 +0200

    made mobile page config view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,37 @@
 class PagesController < ApplicationController
+before_action :set_variant
+before_action :format
+
+
   def index
     @user = User.new
+  end
+
+
+private
+
+  def format
+    respond_to do |format|
+      format.html(&:phone)
+      # do |html|
+      # html.phone
+      # html.tablet
+      # end
+    end
+  end
+
+  def set_variant
+    case request.user_agent
+    when /iPhone/i
+      request.variant = :phone
+    when /Android/i && /mobile/i
+      request.variant = :phone
+    when /Windows Phone/i
+      request.variant = :phone
+    # when /Android/i
+    #   request.variant = :tablet
+    # when /iPad/i
+    #   request.variant = :tablet
+    end
   end
 end
